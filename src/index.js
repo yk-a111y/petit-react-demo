74508c88d24aedfaf4844957eaf63678143e072b
yukai06 <yukai06@58.com>
2025-04-10 20:26:59 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const jsx = (
+  <div>
+    <h1>Hello World</h1>
+  </div>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log("🚀 ~ jsx:", jsx)
+console.log("🚀 ~ React:", React)
+console.log("🚀 ~ ReactDOM:", ReactDOM)
+
